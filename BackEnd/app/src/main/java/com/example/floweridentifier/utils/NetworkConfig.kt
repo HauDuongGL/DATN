d7dfb6fa1b5d7db11d9eab8d19c78dd63fe97a7d
HauDuongGL <dphau29082002@gmail.com>
2026-01-06 00:53:06 +0700

    chore: Update BASE_URL to ngrok endpoint and add interceptor to bypass ngrok warning

--- BackEnd/app/src/main/java/com/example/floweridentifier/utils/NetworkConfig.kt
+++ BackEnd/app/src/main/java/com/example/floweridentifier/utils/NetworkConfig.kt
@@ -3,7 +3,7 @@ package com.example.floweridentifier.utils
 import com.example.floweridentifier.data.model.User
 
 object NetworkConfig {
-    const val BASE_URL = "https://config.phucanhthanh.com/api/v1/"
+    const val BASE_URL = "https://mozell-conservant-deceivably.ngrok-free.dev/"
 
     // Default chat gpt3 api key, update when open app
     var OPEN_API_KEY = "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"
